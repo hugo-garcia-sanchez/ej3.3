03ac9e7cc57051bc55a560e6e3537b7f833ed1d6
Hugo García Sánchez <hugo.garcia.sanchez@estudiantat.upc.edu>
2024-09-29 21:43:55 +0200

    Add files via upload

---
+++ ejgrupo.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS DDBB;
+CREATE DATABASE DDBB;
+USE DDBB;
+
+CREATE TABLE Usuario (
+	idUsuario INT PRIMARY KEY NOT NULL,
+	nombre VARCHAR (255) NOT NULL,
+	contraseña VARCHAR (255) NOT NULL,
+	puntos INT NOT NULL
+)ENGINE = InnoDB;
+
+CREATE TABLE Mesa (
+	idMesa INT PRIMARY KEY NOT NULL,
+	numJugadores INT NOT NULL,
+	numCartas INT NOT NULL
+)ENGINE = InnoDB;
+
+CREATE TABLE Relaciones (
+	PlayerId INT NOT NULL,
+	MesaId INT NOT NULL,
+FOREIGN KEY (PlayerId) REFERENCES Usuario(idUsuario),
+FOREIGN KEY (MesaId) REFERENCES Mesa(idMesa)
+)ENGINE = InnoDB;
+
+INSERT INTO Usuario VALUES (9,'Hugo','hugo',10);
+INSERT INTO Usuario VALUES (3,'Iker','iker',9);
+INSERT INTO Usuario VALUES (5,'Jordi','jordi',7);
+INSERT INTO Usuario VALUES (7,'Ivan','ivan',13);
+
+INSERT INTO Mesa VALUES (1,4,60);
+INSERT INTO Relaciones VALUES (9,1);
+INSERT INTO Relaciones VALUES (3,1);
+INSERT INTO Relaciones VALUES (5,1);
+INSERT INTO Relaciones VALUES (7,1);
+
+SELECT Mesa.numJugadores FROM Usuario, Mesa, Relaciones WHERE
+	Usuario.nombre = 'Hugo' AND
+	Usuario.idUsuario = Relaciones.PlayerID AND
+	Mesa.idMesa = Relaciones.MesaId;
+
+
+
+
+
+
+
+
+
